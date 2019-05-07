0d1712119eb69c41cfc2e14d6c83e3b1064be038
RajendranEshwaran <rajendranmca2010@gmail.com>
2019-05-07 16:05:49 -0400

    Initial Commit

---
+++ Spotify/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spotify
+//
+//  Created by gomathi saminathan on 5/7/19.
+//  Copyright © 2019 Rajendran Eshwaran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
